c1a341425a96a0826dbd089b6923340fbf10a060
Rowin Bodegom <99056991@mydavinci.nl>
2019-11-22 09:02:03 +0100

    Begin van alles

---
+++ adventure.js
@@ -0,0 +1,6 @@
+var button1 = document.createElement("button");
+document.body.appendChild(button1)
+button1.innerHTML = "kaas";
+
+//var background0 = document.body
+//background0.style.backgroundImage = "url('images/backlevel0.png')";
