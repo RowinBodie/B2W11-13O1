89d1fd2c8366e81d7133df02f5438f100d1f0a5d
Rowin Bodegom <99056991@mydavinci.nl>
2019-11-22 10:58:59 +0100

    Me testing my stuff

--- adventure.js
+++ adventure.js
@@ -1,6 +1,38 @@
-var button1 = document.createElement("button");
-document.body.appendChild(button1)
-button1.innerHTML = "kaas";
+var nummer = 1;
+test();
 
-//var background0 = document.body
-//background0.style.backgroundImage = "url('images/backlevel0.png')";
+function test(){
+    var button1 = document.createElement("button");
+    document.body.appendChild(button1);
+    button1.onclick = function() {selector(nummer)};
+    button1.innerHTML = "test";
+}
+
+function selector(number){
+    plate(number);
+    nummer++;
+}
+
+function plate(number){
+    if(number == 1){
+        var button1 = document.createElement("button");
+        document.body.appendChild(button1);
+        button1.onclick = function() {background(0)};
+        button1.innerHTML = "background";
+    }
+    else if(number == 2){
+        var button2 = document.createElement("button");
+        document.body.appendChild(button2);
+        button2.onclick = function() {background(1)};
+        button2.innerHTML = "button2";
+    }
+}
+
+function background(optionNumber){
+    var options = [
+        "url('images/backlevel0.png')", 
+        "url('images/backlevel1.png')"
+    ];
+    var background = document.body;
+    background.style.background = options[optionNumber];
+}
