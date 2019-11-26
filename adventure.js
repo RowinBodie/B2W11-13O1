4ed43a70d9592b62b1580d29ea6ca7c32b2d973b
Rowin Bodegom <99056991@mydavinci.nl>
2019-11-26 10:10:41 +0100

    Begin Van de game
    Real Update 1

--- adventure.js
+++ adventure.js
@@ -1,38 +1,61 @@
-var nummer = 1;
-test();
+var username;
+background(0);
+var nextPressed = 0;
+var text;
+var text2;
+var next;
 
-function test(){
-    var button1 = document.createElement("button");
-    document.body.appendChild(button1);
-    button1.onclick = function() {selector(nummer)};
-    button1.innerHTML = "test";
+var introKnop = document.createElement("button");
+document.body.appendChild(introKnop);
+introKnop.id = "introKnop"
+introKnop.onclick = function() {intro()};
+introKnop.innerHTML = "The Begin of The End";
+
+function intro(){
+    background(1);
+    start();
+    nextOne();
 }
 
-function selector(number){
-    plate(number);
-    nummer++;
+function start(){
+    username = prompt("Please enter your username.");
+
+    var textPaper = document.createElement("div");
+    textPaper.className = "text";
+    textPaper.id = "introText" ;
+    document.body.appendChild(textPaper);
+
+    introKnop.style.display = "none";
+
+    next = document.createElement("button");
+    next.id = "Next";
+    next.innerHTML = "Next";
+    document.getElementById("introText").appendChild(next);
+    next.onclick = function() {nextOne()};
 }
 
-function plate(number){
-    if(number == 1){
-        var button1 = document.createElement("button");
-        document.body.appendChild(button1);
-        button1.onclick = function() {background(0)};
-        button1.innerHTML = "background";
+function nextOne(){
+    nextPressed += 1;
+    if (nextPressed == 1){
+        text = document.createElement("div");
+        text.id = "textIntro";
+        text.innerHTML = "hallo "+username+" welkom tot mijn game";
+        document.getElementById("introText").appendChild(text);
     }
-    else if(number == 2){
-        var button2 = document.createElement("button");
-        document.body.appendChild(button2);
-        button2.onclick = function() {background(1)};
-        button2.innerHTML = "button2";
+    else if(nextPressed == 2){
+        text.style.display = "none";
+        text2 = document.createElement("div");
+        text2.id = "textIntro";
+        text2.innerHTML = username+" u moet overleven en dan dood gaan";
+        document.getElementById("introText").appendChild(text2);
     }
 }
 
 function background(optionNumber){
     var options = [
-        "url('images/backlevel0.png')", 
-        "url('images/backlevel1.png')"
+        "url('images/backlevel0.png')",
+        "url('images/backlevel1.png')", 
+        "url('images/backlevel2.png')"
     ];
-    var background = document.body;
-    background.style.background = options[optionNumber];
+    document.body.style.background = options[optionNumber];
 }
